5f33673ab87d7041363d8bb3cf53bd15c61c30df
Arturo <arturodamianvargas1996@gmail.com>
2023-11-30 23:33:40 -0600

    updated server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+// Importing modules and dependencies//
+
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers: require("./utils/helpers") });
+
+// Creating app and express port //
+const app = express();
+const PORT = process.env.PORT || 3001;
+
