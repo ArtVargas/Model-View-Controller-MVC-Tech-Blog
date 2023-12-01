340a2c5edd1db5b557d3c70c12e4197adb4cd99d
Arturo <arturodamianvargas1996@gmail.com>
2023-11-30 19:54:05 -0600

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
-//Update later //
+const sequelize= process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.Db_USER, process.env.DB_PASSWORD, { // This will be for local
+
+host: 'localhost',
+dialect: 'mysql',
+dialectOptions: {
+decimalNumbers: true,
+},
+});
 
 module.exports = sequelize;
